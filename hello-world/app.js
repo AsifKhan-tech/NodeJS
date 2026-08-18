2fe49d473a9b0a6ad19f1c7b7a4fe2964d37486d
Asif Khan <asifjamshed4@gmail.com>
2026-08-19 00:17:33 +0530

    learn about argv argument of process object

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,1 +1,25 @@
+/* 
 console.log(process.argv);
+* process provides arguments in argv array
+* arguments:- node app.js, node app.js Asif, node app.js server
+*                  [====]       [==========]      [============]
+
+*/
+
+/*
+ * Create a greetings app, greet to the user according to the time (morning, noon, afternoon, evening)
+ */
+
+const name = process.argv[2];
+
+const hours = new Date().getHours(); //it's in 24 hour format
+console.log("Hours:", hours);
+
+function getGreeting(hours) {
+  if (hours < 4 || hours > 19) return "Good night";
+  if (hours < 9) return "Good morning";
+  if (hours < 16) return "Good afternoon";
+  return "Good evening";
+}
+const greetings = getGreeting(hours);
+console.log(`${greetings}, ${name}!`);
