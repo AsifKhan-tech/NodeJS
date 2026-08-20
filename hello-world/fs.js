9e084ad6d1d497608d495925642489cf18829993
Asif Khan <asifjamshed4@gmail.com>
2026-08-21 00:22:10 +0530

    create async file handling in fs.js and add async.txt

--- hello-world/fs.js
+++ hello-world/fs.js
@@ -2,10 +2,33 @@ import * as fs from "node:fs";
 
 function createFile(pathname) {
   //Sync API
+  /** 
   fs.writeFileSync(pathname, "Create file synchronously!\n");
   // fs.writeFileSync(pathname, "Hello JavaScript!"); replace file &  create new file with new content
   fs.appendFileSync(pathname, "Hello Node.js");
   console.log("File has been created!");
+*/
+  //Async API
+  //Error first callbacks - When we use callback in Nodejs, the first parameter in the callback is error
+  fs.writeFile(pathname, "Create file Asynchronously!\n", (err) => {
+    if (err) {
+      console.log("Something went wrong while creating file!");
+      return;
+    } else {
+      fs.appendFile(pathname, "Hello JavaScript!\n", (err) => {
+        if (err) {
+          console.log("Something went wrong while creating file!");
+          return;
+        } else {
+          console.log("File has been updated Asynchronously!");
+        }
+      });
+
+      console.log("File has been created Asynchronously!");
+    }
+  });
+
+  console.log("File operation done!");
 }
 
-createFile("./hello.txt");
+createFile("./async.txt");
