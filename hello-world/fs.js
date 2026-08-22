4fd46cf45fa4722932616fda565bc7f4f9e2b5b8
Asif Khan <asifjamshed4@gmail.com>
2026-08-22 23:59:04 +0530

    refactor fs.js to comment out synchronous and asynchronous file handling code

--- hello-world/fs.js
+++ hello-world/fs.js
@@ -1,6 +1,8 @@
-import * as fs from "node:fs";
+// import * as fs from "node:fs";
+import * as fs from "node:fs/promises";
 
 function createFile(pathname) {
+  //promises
   //Sync API
   /** 
   fs.writeFileSync(pathname, "Create file synchronously!\n");
@@ -10,25 +12,23 @@ function createFile(pathname) {
 */
   //Async API
   //Error first callbacks - When we use callback in Nodejs, the first parameter in the callback is error
-  fs.writeFile(pathname, "Create file Asynchronously!\n", (err) => {
-    if (err) {
-      console.log("Something went wrong while creating file!");
-      return;
-    } else {
-      fs.appendFile(pathname, "Hello JavaScript!\n", (err) => {
-        if (err) {
-          console.log("Something went wrong while creating file!");
-          return;
-        } else {
-          console.log("File has been updated Asynchronously!");
-        }
-      });
-
-      console.log("File has been created Asynchronously!");
-    }
-  });
-
-  console.log("File operation done!");
+  // fs.writeFile(pathname, "Create file Asynchronously!\n", (err) => {
+  //   if (err) {
+  //     console.log("Something went wrong while creating file!");
+  //     return;
+  //   } else {
+  //     fs.appendFile(pathname, "Hello JavaScript!\n", (err) => {
+  //       if (err) {
+  //         console.log("Something went wrong while creating file!");
+  //         return;
+  //       } else {
+  //         console.log("File has been updated Asynchronously!");
+  //       }
+  //     });
+  //     console.log("File has been created Asynchronously!");
+  //   }
+  // });
+  // console.log("File operation done!");
 }
 
 createFile("./async.txt");
