159eb13cd7b8b51b1818a92dba137eb276f2c2c4
Asif Khan <asifjamshed4@gmail.com>
2026-08-23 00:00:05 +0530

    comment out createFile function and its usage in fs.js

--- hello-world/fs.js
+++ hello-world/fs.js
@@ -1,34 +1,34 @@
 // import * as fs from "node:fs";
 import * as fs from "node:fs/promises";
 
-function createFile(pathname) {
-  //promises
-  //Sync API
-  /** 
+// function createFile(pathname) {
+//promises
+//Sync API
+/** 
   fs.writeFileSync(pathname, "Create file synchronously!\n");
   // fs.writeFileSync(pathname, "Hello JavaScript!"); replace file &  create new file with new content
   fs.appendFileSync(pathname, "Hello Node.js");
   console.log("File has been created!");
 */
-  //Async API
-  //Error first callbacks - When we use callback in Nodejs, the first parameter in the callback is error
-  // fs.writeFile(pathname, "Create file Asynchronously!\n", (err) => {
-  //   if (err) {
-  //     console.log("Something went wrong while creating file!");
-  //     return;
-  //   } else {
-  //     fs.appendFile(pathname, "Hello JavaScript!\n", (err) => {
-  //       if (err) {
-  //         console.log("Something went wrong while creating file!");
-  //         return;
-  //       } else {
-  //         console.log("File has been updated Asynchronously!");
-  //       }
-  //     });
-  //     console.log("File has been created Asynchronously!");
-  //   }
-  // });
-  // console.log("File operation done!");
-}
+//Async API
+//Error first callbacks - When we use callback in Nodejs, the first parameter in the callback is error
+// fs.writeFile(pathname, "Create file Asynchronously!\n", (err) => {
+//   if (err) {
+//     console.log("Something went wrong while creating file!");
+//     return;
+//   } else {
+//     fs.appendFile(pathname, "Hello JavaScript!\n", (err) => {
+//       if (err) {
+//         console.log("Something went wrong while creating file!");
+//         return;
+//       } else {
+//         console.log("File has been updated Asynchronously!");
+//       }
+//     });
+//     console.log("File has been created Asynchronously!");
+//   }
+// });
+// console.log("File operation done!");
+// }
 
-createFile("./async.txt");
+// createFile("./async.txt");
