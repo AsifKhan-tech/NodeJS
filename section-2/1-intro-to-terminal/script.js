59d7575ffb93ca0e8a9bbc578a4b0059a992a8ce
Asif Khan <asifjamshed4@gmail.com>
2026-08-28 23:58:38 +0530

    Update script.js to correct terminal command descriptions and enhance directory explanations

--- section-2/1-intro-to-terminal/script.js
+++ section-2/1-intro-to-terminal/script.js
@@ -8,8 +8,24 @@ console.log("Start backend journey!");
  * we can also print these variables in terminal like this- echo $name
  * run the script / (.sh) file:- path<filename.sh>
  *** It will not directly run in linux system, you will have to give permission(chmod +x filename.sh)
+
  * echo - can print the content in terminal like (console.log("content"))
- * pwd - print the working directory from the parent
+ * pwd - print the working directory from the root
  * whoami - give the current user of the system
+ * cd - change directory
+ * ls - list the content
+
  *
+ */
+
+/**
+ * / :- root directory(top level / absoloute top) Starting point of the entire file system. Everything lives under this.
+ * Access - Only root user / administrator has full permission to change files here.
+ * Purpose - Hold all system files, programms, settings and other directories like /bin, /etc, /home
+ */
+
+/**
+ * ~ :- home directory - Your personal workspace where you keep your own files, downloads, and custom settings.
+ * Access - Own your home directory & have full control over the files inside it.
+ * purpose - Keep your personal documents safe away from vital system files.
  */
