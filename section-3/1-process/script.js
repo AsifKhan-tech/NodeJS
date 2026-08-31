6235edc0be2385c5b140aedc6fdda6bdffea9d61
Asif Khan <asifjamshed4@gmail.com>
2026-08-31 19:47:25 +0530

    Refactor script.js to assign process to a variable and update documentation comments

--- section-3/1-process/script.js
+++ section-3/1-process/script.js
@@ -1,6 +1,8 @@
-console.log("PROCESS: ", process);
+const a = process;
+console.log("PROCESS: ", a);
 
 /**
  * lscpu - return the system configuration
  * nproc - return the logical cores / threads
- *  */
+ * process can create / start a thread and also use it too
+ * */
