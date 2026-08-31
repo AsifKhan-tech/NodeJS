27392434febae877ab356ddbabcd3879be4f7bc2
Asif Khan <asifjamshed4@gmail.com>
2026-09-01 00:36:46 +0530

    Increase loop limits in loop-1.js, loop-2.js, and loop-3.js for improved performance

--- section-3/2-threads/loop-2.js
+++ section-3/2-threads/loop-2.js
@@ -1,7 +1,7 @@
 console.log(`Loop-2 started`);
 
-for (let i = 0; i < 10_0000000; i++) {
-  if (i % 40_0000000 === 0) {
+for (let i = 0; i < 10_000000000; i++) {
+  if (i % 40_00000000 === 0) {
     console.log("Loop-2", i);
   }
 }
