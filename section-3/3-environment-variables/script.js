10b737a19ac10388efa9050e4dc2323eb8b31fa4
Asif Khan <asifjamshed4@gmail.com>
2026-09-01 23:41:13 +0530

    Add .bashrc to set environment variable and update script.js documentation

--- section-3/3-environment-variables/script.js
+++ section-3/3-environment-variables/script.js
@@ -2,5 +2,7 @@ const environmentVariable = process.env;
 console.log(`Environment Variable: ${environmentVariable}`);
 
 /**
- * Environment variables :-
+ * Environment variables :- It's a pair of key and value pairs of strings that are used to configure the behavior of applications and services. They are typically set outside of the application code and can be accessed by the application at runtime. Environment variables can be used to store sensitive information, such as API keys or database credentials, as well as configuration settings that may vary between different environments (e.g., development, testing, production).
+ *
+ * To create an environment variable, we can use (export variableName=value) in terminals or command prompt. Jab bhi uska (process ka) child process run hoga, usko ye environment variable pass kar diya jayega by operating system.
  */
