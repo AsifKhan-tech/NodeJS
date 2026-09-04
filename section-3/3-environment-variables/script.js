d434be4e4d5ea51251e5a38eec417f57766d53ba
Asif Khan <asifjamshed4@gmail.com>
2026-09-04 23:34:25 +0530

    Comment out unused environment variable code in script.js for clarity

--- section-3/3-environment-variables/script.js
+++ section-3/3-environment-variables/script.js
@@ -1,9 +1,20 @@
-import { exec } from "node:child_process";
+// import { exec } from "node:child_process";
 
-exec(`test="Node.js"`);
+// process.env.TEST = "Node.js set from code";
+
+// console.log(process.env.TEST);
+// process.env.TEST = "Node.js set from code";
+
+// exec("echo $TEST", (error, stdout) => {
+//   console.log(stdout); // Node.js set from code
+// });
+
+// // export TEST="Node.js set from terminal"
+
+// exec(`test="Node.js set from code"`);
 
 const environmentVariable = process.env;
-console.log(`Environment Variable: ${environmentVariable}`);
+console.log("Environment Variable: ", environmentVariable);
 
 /**
  * Environment variables :- It's a pair of key and value pairs of strings that are used to configure the behavior of applications and services. They are typically set outside of the application code and can be accessed by the application at runtime. Environment variables can be used to store sensitive information, such as API keys or database credentials, as well as configuration settings that may vary between different environments (e.g., development, testing, production).
