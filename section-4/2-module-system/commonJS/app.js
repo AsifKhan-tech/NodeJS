5389405b69ca6a6ce50c4311d920a3f98e4cbf19
Asif Khan <asifjamshed4@gmail.com>
2026-09-18 23:45:14 +0530

    Fix comment block formatting and enhance documentation in app.js

--- section-4/2-module-system/commonJS/app.js
+++ section-4/2-module-system/commonJS/app.js
@@ -1,9 +1,18 @@
+/*
 const sum = require("./sum");
 const product = require("./product");
 
 console.log("Sum: ", sum(1, 2, 3, 4, 5));
 console.log("Product: ", product(1, 2, 3, 4, 5));
+/*
 
 /**
  * When the code is growing, and any particular piece ofcode performs a specific task, split that code into multiple files, that files can be said as modules on bird's eye view.
  */
+
+/**
+ *require is a function that takes an argument (a string that would be the path of the file(module)).
+ * It searches for the file according to the provided path, and if it finds the file, it executes the code in that file and returns the value of module.exports object.
+ * If doesn't find the file by giving path, it throws an error.
+ */
+console.log(typeof require);
