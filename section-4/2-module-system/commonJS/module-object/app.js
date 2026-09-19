0916e5a2887e379b6d5c2fc917165f71b676cc00
Asif Khan <asifjamshed4@gmail.com>
2026-09-19 23:35:44 +0530

    Add runtime arguments for module preloading and enhance documentation in app.js and math.js

--- section-4/2-module-system/commonJS/module-object/app.js
+++ section-4/2-module-system/commonJS/module-object/app.js
@@ -1,6 +1,7 @@
 const math = require("./math.js");
 /**
  * All the required modules will add in the childern array of the module object.
+ * isPreloading - a setter function. If there is a need to preload a module, we can do : node --require <filename> <require_filename> | The filename where the module is required. By default it's false.
  */
 
-console.log(module);
+// console.log(module);
--- section-4/2-module-system/commonJS/module-object/math.js
+++ section-4/2-module-system/commonJS/module-object/math.js
@@ -1,1 +1,3 @@
 module.exports = 90;
+
+console.log(module.isPreloading);
