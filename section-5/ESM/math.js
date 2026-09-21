bc089aa2b30aa95c5934bb5d424b6b62a36ffb04
Asif Khan <asifjamshed4@gmail.com>
2026-09-21 23:51:54 +0530

    Enhance math.js with console log for num and update documentation on module file extensions

--- section-5/ESM/math.js
+++ section-5/ESM/math.js
@@ -1,2 +1,8 @@
 //for imorting "named" export, we use "{} curly braces"
 import { num } from "./app.js";
+
+console.log(num);
+
+/**
+ * If there is use of both modules(commomn JS, ESM) in the project, we can have file extension for common JS is index.cjs and for ESM index.mjs
+ */
