8aa09057e0851bb3a77e276d5818f4abbc1304da
Asif Khan <asifjamshed4@gmail.com>
2026-09-21 23:58:07 +0530

    Refactor module exports and imports: move named export to math.js, update app.js to import num, and add package.json for module configuration

--- section-5/ESM/app.js
+++ section-5/ESM/app.js
@@ -1,3 +1,10 @@
 //ES6 module
 //named export
-export const num = 1;
+
+//for imorting "named" export, we use "{} curly braces"
+import { num } from "./math.js";
+console.log(num);
+
+/**
+ * If there is use of both modules(commomn JS, ESM) in the project, we can have file extension for common JS is index.cjs and for ESM index.mjs
+ */
