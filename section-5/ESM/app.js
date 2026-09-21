31f7ca7bcf448a995f65df5a92ee50bc68540c76
Asif Khan <asifjamshed4@gmail.com>
2026-09-22 00:25:58 +0530

    Refactor app.js and math.js: comment out named export in math.js and update app.js to reflect changes in module imports

--- section-5/ESM/app.js
+++ section-5/ESM/app.js
@@ -2,10 +2,20 @@
 //named export
 
 //for imorting "named" export, we use "{} curly braces"
-import { num } from "./math.js";
-console.log(num);
+// import { num } from "./math.js";
+// console.log(num);
 
+//for imorting "default" export, we don't use "{} curly braces"
+
+import num, { newNum } from "./math.js";
+console.log(num, newNum);
+
+var name = "Ayan";
+// number = 22;
 /**
  * If there is use of both modules(commomn JS, ESM) in the project, we can have file extension for common JS is index.cjs and for ESM index.mjs
  */
-//In ESM,
+//In ESM, NodeJS creates a module scope instead of local scope and the variables created and imported in the file primarly fall in the module scope
+
+//By default, there is "use strict" enabled in the script
+//putting .js extension is mandatory in the imported file
--- section-5/ESM/math.js
+++ section-5/ESM/math.js
@@ -1,2 +1,6 @@
 //named export
-export const num = 1;
+// export const num = 1;
+export const newNum = 12;
+//defalut export
+const num = 1;
+export default num;
