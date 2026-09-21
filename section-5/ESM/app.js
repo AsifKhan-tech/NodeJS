d04807f92c1c76e5747c147f9bad75c7df57986b
Asif Khan <asifjamshed4@gmail.com>
2026-09-22 00:00:09 +0530

    Add comment to clarify ESM usage in app.js

--- section-5/ESM/app.js
+++ section-5/ESM/app.js
@@ -8,3 +8,4 @@ console.log(num);
 /**
  * If there is use of both modules(commomn JS, ESM) in the project, we can have file extension for common JS is index.cjs and for ESM index.mjs
  */
+//In ESM,
