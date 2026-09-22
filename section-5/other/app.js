3eb0d33acb49b04f0027a5b6abf25ff5ffb7890e
Asif Khan <asifjamshed4@gmail.com>
2026-09-22 23:02:14 +0530

    Add math.js: export num constant and update app.js to log import.meta

--- section-5/other/app.js
+++ section-5/other/app.js
@@ -1,3 +1,6 @@
+import { num } from "./math.js";
+console.log(import.meta);
+
 /**
  * How to access __filename and __dirname in ESM
  * In Node.js, import.meta is a built-in object that provides context-specific metadata about the current ECMAScript module (ESM).
@@ -5,6 +8,6 @@
  * context-specific means the information inside the object changes depending on exactly which file is reading it.
  */
 
-console.log(import.meta);
 //It has several properties:- it provides url property ans resolve method in the browser
-//url: this is the url where the module is load.
+//url: which url load the module.
+//resolve: we can load more modules by loading the path of another modules.
