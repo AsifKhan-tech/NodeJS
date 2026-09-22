5b6863078a60219446cfa6769b7c253898e409dc
Asif Khan <asifjamshed4@gmail.com>
2026-09-22 23:12:14 +0530

    Update app.js, index.html, and math.js: refine comments and enhance metadata logging

--- section-5/other/app.js
+++ section-5/other/app.js
@@ -2,12 +2,12 @@ import { num } from "./math.js";
 console.log(import.meta);
 
 /**
- * How to access __filename and __dirname in ESM
+ * How to access filename and dirname in ESM
  * In Node.js, import.meta is a built-in object that provides context-specific metadata about the current ECMAScript module (ESM).
  * It's avialable in Browser and NodeJS
  * context-specific means the information inside the object changes depending on exactly which file is reading it.
  */
 
 //It has several properties:- it provides url property ans resolve method in the browser
-//url: which url load the module.
+//url: which url load the module. It's HTTPS path in browser and FTP(file) path in NodeJS
 //resolve: we can load more modules by loading the path of another modules.
--- section-5/other/math.js
+++ section-5/other/math.js
@@ -1,1 +1,3 @@
 export const num = 1;
+const { filename, dirname } = import.meta;
+console.log(filename, dirname);
