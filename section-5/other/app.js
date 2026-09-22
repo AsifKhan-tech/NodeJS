b8f4d713ff673d74e5193822fb963fe53ec66be1
Asif Khan <asifjamshed4@gmail.com>
2026-09-22 22:50:13 +0530

    Add index.html: create a basic HTML file to demonstrate ESM usage with app.js

--- section-5/other/app.js
+++ section-5/other/app.js
@@ -1,6 +1,7 @@
 /**
  * How to access __filename and __dirname in ESM
  * In Node.js, import.meta is a built-in object that provides context-specific metadata about the current ECMAScript module (ESM).
+ * It's avialable in Browser and NodeJS
  * context-specific means the information inside the object changes depending on exactly which file is reading it.
  */
 
