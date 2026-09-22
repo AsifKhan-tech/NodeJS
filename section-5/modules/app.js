bde9224824cb62def36266d18d5362e8e224d1c0
Asif Khan <asifjamshed4@gmail.com>
2026-09-22 23:42:51 +0530

    Add timer.js: implement a simple loop to log numbers and update app.js comments for clarity

--- section-5/modules/app.js
+++ section-5/modules/app.js
@@ -1,2 +1,10 @@
 //Common JS
+// const timer = require("./timer.js");
+//require function itself searches the file in the filesystem and reads it. JS Main thread will busy do it
 //These load synchronously
+
+//ESM
+import timer from "./timer.js";
+//Now the work of loading, the JS main thread will not do it.
+//These load Asynchronously: As JS engine reads the files asynchronously while if we read the file, we will JS for it that is synchronously
+//
