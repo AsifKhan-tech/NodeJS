52ee53b6e8ea9b250d0db88e07e4b0766cfeaad9
Asif Khan <asifjamshed4@gmail.com>
2026-09-24 23:27:42 +0530

    Update app.js: reorganize import statements for native and third-party modules

--- section-5/module-types/app.js
+++ section-5/module-types/app.js
@@ -1,4 +1,12 @@
 //native module
 import fs from "node:fs";
+import net from "node:net";
+import dgram from "node:dgram";
+import http from "node:http";
+import https from "node:https";
 //user module
 import { num } from "./math.js";
+
+//third-party modules
+import dotenv from "dotenv";
+dotenv();
