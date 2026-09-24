76278627c7c002d97df65bbd436a326eee4b919d
Asif Khan <asifjamshed4@gmail.com>
2026-09-24 23:56:16 +0530

    Update app.js: add commonjs import for num from maths

--- section-5/module-types/app.js
+++ section-5/module-types/app.js
@@ -5,7 +5,11 @@ import dgram from "node:dgram";
 import http from "node:http";
 import https from "node:https";
 
+//ESM
 import { num } from "maths";
+//commonjs
+const { num } = require("maths");
+
 //user module
 // import { num } from "math";
 console.log(num);
