51cc03265ce16d8d6788013c24ae7647d932f1e5
Asif Khan <asifjamshed4@gmail.com>
2026-09-25 23:44:53 +0530

    Add shebang to app.js and update console log message with explanation

--- section-6/shebang/app.js
+++ section-6/shebang/app.js
@@ -1,1 +1,6 @@
-console.log("shebang");
+#!/usr/bin/env node
+
+console.log("File with shebang");
+/**
+ * When I'm running this file doing ./app.js, it is running bash app.js until I didn't add shebang. After adding shebang, it is running node app.js. So, shebang is used to tell the OS which interpreter to use to run the script.
+ */
