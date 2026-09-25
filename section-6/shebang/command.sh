dea8ffc8972fc4ffa3e1c164904aab86c64ea4b2
Asif Khan <asifjamshed4@gmail.com>
2026-09-25 23:33:07 +0530

    Add command.sh: create a new script with shebang and explanatory comments

---
+++ section-6/shebang/command.sh
@@ -0,0 +1,5 @@
+# A shebang, hashbang, pound-bang, or sharp-bang is the character sequence at the absolute beginning of a script. It acts as an interpreter directive, telling the operating system's kernel exactly which program to use to parse and execute the rest of the file.
+
+# npm and npx both use the shebang
+
+#!/bin/env bash
