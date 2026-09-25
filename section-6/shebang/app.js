500d61ad35d9a8037c5227366d90a67d3f796392
Asif Khan <asifjamshed4@gmail.com>
2026-09-25 23:48:08 +0530

    Update app.js: add explanatory comments for the shebang directive

--- section-6/shebang/app.js
+++ section-6/shebang/app.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
+/**
+ * This shebang directive is now a portable shebang because it uses the env utility. It dynamically searches the user's $PATH to find where the interpreter is installed / located.
+ */
+
 console.log("File with shebang");
 /**
  * When I'm running this file doing ./app.js, it is running bash app.js until I didn't add shebang. After adding shebang, it is running node app.js. So, shebang is used to tell the OS which interpreter to use to run the script.
