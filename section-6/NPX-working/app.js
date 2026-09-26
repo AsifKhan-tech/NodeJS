e2c43a379fcabdc08fcfb81f65462d71b9f4d097
Asif Khan <asifjamshed4@gmail.com>
2026-09-26 23:17:30 +0530

    Add app.js and package.json: create main application file and package configuration

---
+++ section-6/NPX-working/app.js
@@ -0,0 +1,7 @@
+/**
+ * npx searches a file and executes it
+ *
+ * npx <package_name>
+ * NPX Searching steps:
+ * Firstly search package.json file in current working directory and see that what is the value of "name" key, if name key is find, after it searches for "bin" key it means that which file has to execute
+ */
