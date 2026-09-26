3de26a7fe98c07f71ec8248d0c310e09a4d5900d
Asif Khan <asifjamshed4@gmail.com>
2026-09-26 23:53:16 +0530

    Update app.js: add additional details on globally installed packages for NPX

--- section-6/NPX-working/app.js
+++ section-6/NPX-working/app.js
@@ -10,6 +10,9 @@
  * Second step it searches .bin folder inside it hello named file that all inside in node_modules folder in current working directory and execute it
  *
  * Third step it searches the given package is globally installed in the system :<npx hello>
+ *
+ * Where Globally installed packages found: pnpm root -g gives it: /home/asifkhan/snap/code/241/.local/share/pnpm/global/v11
+ * npm root -g: /home/asifkhan/.nvm/versions/node/v24.15.0/lib/node_modules
  */
 
 console.log("NPX working...");
