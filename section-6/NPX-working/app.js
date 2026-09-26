fa2fd99f1b743a1decd25bce964840d0f37c1482
Asif Khan <asifjamshed4@gmail.com>
2026-09-26 23:27:48 +0530

    Update app.js: change file permissions to executable and add shebang

--- section-6/NPX-working/app.js
+++ section-6/NPX-working/app.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 /**
  * npx searches a file and executes it
  *
@@ -5,3 +7,5 @@
  * NPX Searching steps:
  * Firstly search package.json file in current working directory and see that what is the value of "name" key, if name key is find, after it searches for "bin" key it means that which file has to execute
  */
+
+console.log("NPX working...");
